0d1bf7870903ecbd4468f738c851300467b24135
blah <rs@localhost.localdomain>
2013-02-07 16:18:02 +0000

    build.sh fix

--- project1/build.sh
+++ project1/build.sh
@@ -1,1 +1,1 @@
-mkdir -p build && mkdir -p bin && cd build && cmake ../ && make && make install DESTDIR=~/xlibproj/project1/bin
+mkdir -p build && mkdir -p bin && cd build && cmake ../ && make && make install DESTDIR=../bin
--- projectTransparent/build.sh
+++ projectTransparent/build.sh
@@ -1,1 +1,1 @@
-mkdir -p build && mkdir -p bin && cd build && cmake ../ && make && make install DESTDIR=~/xlibproj/projectTransparent/bin
+mkdir -p build && mkdir -p bin && cd build && cmake ../ && make && make install DESTDIR=../bin
